ce194f8c93b2a8e98f1fc153cc62f5312204e3f3
Chen Dong <dchen15@dow.com>
2018-05-02 16:20:22 +0800

    add bootstrap

--- angular-exercise/src/app/app.module.ts
+++ angular-exercise/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EventDetailsComponent } from './event/event-details/event-details.component';
+import { EventListComponent } from './event/event-list/event-list.component';
+import { TopMenuComponent } from './nav/top-menu/top-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    EventListComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule
