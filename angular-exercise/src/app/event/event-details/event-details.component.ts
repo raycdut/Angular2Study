18423f916ba299ac3a738db94d9c0fd423887ef8
Chen Dong <dchen15@dow.com>
2018-05-02 16:55:24 +0800

    add nav menu

--- angular-exercise/src/app/event/event-details/event-details.component.ts
+++ angular-exercise/src/app/event/event-details/event-details.component.ts
@@ -1,22 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({selector: 'app-event-details', templateUrl: './event-details.component.html', styleUrls: ['./event-details.component.css']})
 export class EventDetailsComponent implements OnInit {
 
-  event;
+  @Input()event : any;
   constructor() {}
 
-  ngOnInit() {
-    this.event = {
-      name: 'ngConf 2025',
-      date: '3/1/2025',
-      time: '8am',
-      location: {
-        address: '123 Main St',
-        city: 'Salt Lake City, UT',
-        country: 'USA'
-      }
-    }
-  }
-
+  ngOnInit() {}
 }
--- angular-exercise/src/app/event/event-list/event-list.component.ts
+++ angular-exercise/src/app/event/event-list/event-list.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent implements OnInit {
-
+event:any;
   constructor() { }
 
   ngOnInit() {
+    this.event = {
+      name: 'ngConf 2025',
+      date: '3/1/2025',
+      time: '8am',
+      location: {
+        address: '123 Main St',
+        city: 'Salt Lake City, UT',
+        country: 'USA'
+      }
   }
-
+  }
 }
